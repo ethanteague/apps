1ad007b40083c953c3927e477e6485107cf4d042
Ethan Teague <ethan.teague@civicactions.com>
2016-01-30 15:04:15 -0500

    Cleaning up lotto retrun values.

--- lotto/client/client.js
+++ lotto/client/client.js
@@ -7,16 +7,29 @@
   // Templates
   Template.pollGrab.helpers({
     data: function() {
-      var title = [];
-      Meteor.call('stream', function(err, res) {
+      var title = [],
+        game = [],
+        winnum = [],
+        drawdate = [];
+
+      Meteor.call("stream", function(err, res) {
         var xml = $.parseXML(res.content),
-        xml = $(xml);
-        xml.find('description').each(function() {
-          title.push(this.innerHTML);
+          xml = $(xml);
+        xml.find("item").each(function(i, j) {
+          game = {
+            "game": $(j).attr("game")
+          };
+          winnum = {
+            "winnum": $(j).attr("winnum")
+          };
+          drawdate = {
+            "drawdate": $(j).attr("windd")
+          };
+          title.push(game, winnum, drawdate);
         });
-        Session.set('games', title);
-        console.log(title[0]);
+        Session.set("games", title);
+        console.log(title);
       });
-      return Session.get('games');
+      return Session.get("games");
     }
   });
